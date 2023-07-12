039fd7c652595dd7ff4ee80d574e2b24e9e604f2
ChimiB <135072431+ChimiB@users.noreply.github.com>
2023-07-12 14:03:50 -0300

    final(creo)

--- java.js
+++ java.js
@@ -1,8 +1,9 @@
 function showAlert() {
-    alert("TE AMO");
+    alert("TE AMO &#x1F49C;✨");
   }
   document.querySelector("button.button-menu-toggle")
   .addEventListener("click", function () {
     document.querySelector(".nav-links").
       classList.toggle("nav-links-responsive")
   })
+
